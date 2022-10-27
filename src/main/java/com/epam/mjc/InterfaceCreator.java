c125a2a9aca642f0918a4a32b35ee16116dfc494
Alex-Kross <artemkirlanovia@gmail.com>
2022-10-28 00:33:45 +0300

    task completed

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,5 +1,7 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
@@ -11,22 +13,70 @@ import java.util.function.Supplier;
 public class InterfaceCreator {
 
     public Predicate<List<String>> isValuesStartWithUpperCase() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list -> {
+            boolean result = true;
+            for (String elList : list) {
+                char firstLetter = elList.charAt(0);
+                if (firstLetter < 65 || firstLetter > 90) {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        };
     }
 
     public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list -> {
+            List<Integer> newList = new ArrayList<>();
+            for (int elList : list) {
+                if (elList % 2 == 0) {
+                    newList.add(elList);
+                }
+            }
+            list.addAll(newList);
+        };
     }
 
     public Supplier<List<String>> filterCollection(List<String> values) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return () -> {
+            List<String> list = new ArrayList<>();
+            for (String sequence: values) {
+                if (sequence.endsWith(".") && sequence.charAt(0) >= 65 && sequence.charAt(0) <= 90) {
+                    String[] words = sequence.split(" ");
+                    int countWord = 0;
+                    for (String word : words) {
+                        char firstLetter = word.charAt(0);
+                        if (firstLetter >= 65 && firstLetter <= 90 || firstLetter >= 97 && firstLetter <= 122) {
+                            countWord++;
+                        }
+                    }
+                    if (countWord > 3) {
+                        list.add(sequence);
+                    }
+                }
+            }
+            return list;
+        };
+
     }
 
     public Function<List<String>, Map<String, Integer>> stringSize() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list -> {
+            Map<String, Integer> map = new HashMap<>();
+            for (String str : list) {
+                map.put(str, str.length());
+            }
+            return map;
+        };
     }
 
     public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (list1, list2) -> {
+            List<Integer> newList = new ArrayList<>();
+            newList.addAll(list1);
+            newList.addAll(list2);
+            return  newList;
+        };
     }
 }
